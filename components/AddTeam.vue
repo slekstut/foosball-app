4031223394ed87adb2a653521465b7ee939197c8
Šarūnas <52723739+slekstut@users.noreply.github.com>
2022-08-06 14:27:09 +0300

    success upload img to firestore storage

--- components/AddTeam.vue
+++ components/AddTeam.vue
@@ -1,43 +1,35 @@
 <template>
     <Modal v-if="showModal">
-      <template #header>
-        <h2>
-          Add A New Team
-        </h2>
-      </template>
-      <template #body>
-        <form>
-          <label for="name">Team Name:</label>
-          <input type="text" id="name" v-model="newTeam.name" placeholder="Enter team title">
-          
-          <label for="imageUrl">Team Logo</label>
-          <button
-            v-if="!newTeam.imageUrl"
-            @click="launchImageFile"
-            :disabled="isUploadingImage"
-            type="button"
-            >
-            {{ isUploadingImage ? 'Uploading...' : 'Upload' }}
-            </button>
-          <input
-            ref="imageFile"
-            @change.prevent="uploadImageFile($event.target.files)"
-            type="file"
-            accept="image/png, image/jpeg"
-            class="hidden"
-            >
+        <template #header>
+            <h2>
+                Add A New Team
+            </h2>
+        </template>
+        <template #body>
+            <form>
+                <label for="name">Team Name:</label>
+                <input type="text" id="name" v-model="newTeam.name" placeholder="Enter team title">
 
-          <label for="player1">Player 1:</label>
-          <input type="text" id="player1" v-model="newTeam.player1" placeholder="Enter a name">
-          <label for="player2">Player 2:</label>
-          <input type="text" id="player2" v-model="newTeam.player2" placeholder="Enter a name">
-          <Button class="submit-team" @click="addTeam, fetchTeams">Submit</Button>
-        </form>
-      </template>
+                <label for="storageRef">Team Logo</label>
+                <button v-if="!newTeam.storageRef" @click="launchImageFile" :disabled="isUploadingImage" type="button">
+                    {{ isUploadingImage ? 'Uploading...' : 'Upload' }}
+                </button>
+                <input ref="imageFile" @change.prevent="uploadImageFile($event.target.files)" type="file"
+                    accept="image/png, image/jpeg" class="hidden">
+
+                <label for="player1">Player 1:</label>
+                <input type="text" id="player1" v-model="newTeam.player1" placeholder="Enter a name">
+                <label for="player2">Player 2:</label>
+                <input type="text" id="player2" v-model="newTeam.player2" placeholder="Enter a name">
+                <Button class="submit-team" @click="addTeam, fetchTeams">Submit</Button>
+            </form>
+        </template>
     </Modal>
 </template>
 
 <script>
+import { getStorage, ref, uploadBytes } from "firebase/storage";
+
 export default {
     data() {
         return {
@@ -57,49 +49,54 @@ export default {
         toggleModal() {
             this.$store.commit("toggleModal");
         },
-        launchImageFile () {
-      // Trigger the file input click event.
-      this.$refs.imageFile.click()
-    },
-    uploadImageFile (files) {
-      if (!files.length) {
-        return
-      }
-      const file = files[0]
+        launchImageFile() {
+            // Trigger the file input click event.
+            this.$refs.imageFile.click()
+        },
+        uploadImageFile(files) {
+            if (!files.length) {
+                return
+            }
+            const file = files[0]
 
-      if (!file.type.match('image.*')) {
-        alert('Please upload an image.')
-        return
-      }
+            if (!file.type.match('image.*')) {
+                alert('Please upload an image.')
+                return
+            }
 
-      const metadata = {
-        contentType: file.type
-      }
+            const metadata = {
+                contentType: file.type
+            }
 
-      this.isUploadingImage = true
+            this.isUploadingImage = true
 
-      // Create a reference to the destination where we're uploading
-      // the file.
-      const storage = this.$firebase.storage()
-      const imageRef = storage.ref(`images/${file.name}`)
+            // Create a reference to the destination where we're uploading
+            // the file.
+            const storage = getStorage();
+            const storageRef = ref(storage, `images/${file.name}`);
 
-      const uploadTask = imageRef.put(file, metadata).then((snapshot) => {
-        // Once the image is uploaded, obtain the download URL, which
-        // is the publicly accessible URL of the image.
-        return snapshot.ref.getDownloadURL().then((url) => {
-          return url
-        })
-      }).catch((error) => {
-        console.error('Error uploading image', error)
-      })
+            // 'file' comes from the Blob or File API
+            uploadBytes(storageRef, file).then((snapshot) => {
+                console.log('Uploaded a blob or file!');
+            });
 
-      // When the upload ends, set the value of the team image URL
-      // and signal that uploading is done.
-      uploadTask.then((url) => {
-        this.newTeam.imageUrl = url
-        this.isUploadingImage = false
-      })
-    },
+            // const uploadTask = storageRef.put(file, metadata).then((snapshot) => {
+            //     // Once the image is uploaded, obtain the download URL, which
+            //     // is the publicly accessible URL of the image.
+            //     return snapshot.ref.getDownloadURL().then((url) => {
+            //         return url
+            //     })
+            // }).catch((error) => {
+            //     console.error('Error uploading image', error)
+            // })
+
+            // When the upload ends, set the value of the team image URL
+            // and signal that uploading is done.
+            // uploadTask.then((url) => {
+            //     this.newTeam.imageUrl = url
+            //     this.isUploadingImage = false
+            // })
+        },
     },
     computed: {
         showModal() {
@@ -111,28 +108,28 @@ export default {
 
 <style scoped lang="scss">
 .modal {
-  form {
-    display: flex;
-    flex-direction: column;
+    form {
+        display: flex;
+        flex-direction: column;
 
-    .submit-team {
-      margin-top: 30px;
-    }
+        .submit-team {
+            margin-top: 30px;
+        }
 
-    label {
-      margin: 12px 0;
-      font-weight: 600;
-    }
+        label {
+            margin: 12px 0;
+            font-weight: 600;
+        }
 
-    input {
-      border: none;
-      border-bottom: 1px solid $primary-400;
-      padding-bottom: 4px;
-    }
+        input {
+            border: none;
+            border-bottom: 1px solid $primary-400;
+            padding-bottom: 4px;
+        }
 
-    input:focus {
-      outline: none;
+        input:focus {
+            outline: none;
+        }
     }
-  }
 }
 </style>
