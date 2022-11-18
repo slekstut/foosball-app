58c692720fb2f8d37b343a3c29d199ce9c4933a2
Šarūnas <52723739+slekstut@users.noreply.github.com>
2022-11-18 20:15:29 +0200

    added component input

---
+++ components/Input.vue
@@ -0,0 +1,53 @@
+<!-- // create input component -->
+<template>
+    <div class="input__wrapper">
+        <label :for="id">{{ label }}</label>
+        <input :type="type" :id="id" v-model="value">
+    </div>
+
+
+</template>
+
+<script>
+export default {
+    props: {
+        label: {
+            type: String,
+            required: true
+        },
+        type: {
+            type: String,
+            required: true
+        },
+        id: {
+            type: String,
+            required: true
+        }
+    },
+    data() {
+        return {
+            value: ''
+        }
+    }
+}
+
+</script>
+
+<style lang="scss">
+.input__wrapper {
+    margin-bottom: 16px;
+    label {
+        display: block;
+        margin-bottom: 8px;
+    }
+    input {
+        width: 100%;
+        padding: 8px;
+        border: 1px solid $primary-300;
+        border-radius: 5px;
+        background-color: $secondary-500;
+        color: $primary-300;
+    }
+}
+
+</style>
--- pages/Login.vue
+++ pages/Login.vue
@@ -1,6 +1,25 @@
 <template>
     <div class="container">
         <Nav></Nav>
-        <h1>login</h1>
+        <h1>Please login</h1>
+
+        <form action="">
+            <!-- import input component -->
+            <Input label="Email" type="email" id="email"></Input>
+            <Input label="Password" type="password" id="password"></Input>
+
+            <!-- add Button component -->
+            <Button>Login</Button>
+
+        </form>
     </div>
 </template>
+
+<script>
+import { auth } from "~/plugins/firebase.js";
+    import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+    import Input from "~/components/Input.vue";
+export default {
+    
+}
+</script>
